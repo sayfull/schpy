b8d740f41c5924a679f1dae88bf4db32895d1d89
sayfull <ilshat.sayfullin@mail.ru>
2013-10-31 16:10:04 +0500

    Create sch.py

---
+++ sch.py
@@ -0,0 +1,52 @@
+def get_month_name(self, month_num):
+       if month_num == 1:
+            return "'JAN'"
+        if month_num == 2:
+            return "'FEB'"
+        if month_num == 3:
+            return "'MAR'"
+        if month_num == 4:
+            return "'APR'"
+        if month_num == 5:
+            return "'MAY'"
+        if month_num == 6:
+            return "'JUN'"
+        if month_num == 7:
+            return "'JUL'"
+        if month_num == 8:
+            return "'AUG'"
+        if month_num == 9:
+            return "'SEP'"
+        if month_num == 10:
+            return "'OCT'"
+        if month_num == 11:
+            return "'NOV'"
+        if month_num == 12:
+            return "'DEC'"
+
+
+def get_month_number(self, month_name):
+        if month_name == "'JAN'":
+            return 1
+        if month_name == "'FEB'":
+            return 2
+        if month_name == "'MAR'":
+            return 3
+        if month_name == "'APR'":
+            return 4
+        if month_name == "'MAY'":
+            return 5
+        if month_name == "'JUN'":
+            return 6
+        if month_name == "'JUL'":
+            return 7
+        if month_name == "'AUG'":
+            return 8
+        if month_name == "'SEP'":
+            return 9
+        if month_name == "'OCT'":
+            return 10
+        if month_name == "'NOV'":
+            return 11
+        if month_name == "'DEC'":
+            return 12
